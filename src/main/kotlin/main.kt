836bfb0547d1df3526f91719b06b1e33563c1b56
Stason <79644303003@yandex.ru>
2023-10-27 01:24:10 +1000

    Inishinal committt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,8 @@
 fun main() {
     val likes = 611
-val op = 100
+
     when  {
         likes % 10 == 1 && likes % 100 != 11 -> println("Понравилось $likes человеку")
-        likes % 100 == 11 -> println("Понравилось $likes людям")
-
         else -> println ("Понравилось $likes людям")
     }
 
